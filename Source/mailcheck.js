e2a2ebed48e16c27171af76623e107fc1eb366b7
Adrian Statescu <mergesortv@gmail.com>
2012-03-21 10:54:47 -0700

    added headers

--- Source/mailcheck.js
+++ Source/mailcheck.js
@@ -1,3 +1,20 @@
+/*
+---
+description: A MooTools plugin that suggests a right domain when your users misspell it in an email address using Levenshtein Distance.
+
+authors:
+- Adrian Statescu (http://thinkphp.ro)
+
+license:
+- MIT-style license
+
+requires:
+ core/1.4.5: '*'
+
+provides: Mailcheck
+...
+*/
+
 var Mailcheck = new Class({
 
     Implements: [Options, Events],
